dc8ca739f3964f90da6ebfbea47f19d9ecf559a8
Patrick Murray <patrick@patmurraydev.com>
2016-11-28 21:13:55 -0500

    testing timezones

--- static/countdown.js
+++ static/countdown.js
@@ -8,7 +8,7 @@
 
 
 /*THESE ARE FOR TESTING -- COMMENT FOR PROD*/
-var hackingStarts = new Date("2016-12-03T14:00:00.000EST");
+var hackingStarts = new Date("2016-12-03T14:00:00.000-0500");
 var hsCountdown = $(".countdown[data-cdNum='1']");
 var devpostEnds = new Date("2016-12-04T01:00:00.000Z");
 var deCountdown = $(".countdown[data-cdNum='2']");
