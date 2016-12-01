bd1946f69eb3f69e9ba755c87b088fb4c9f63c90
Patrick Murray <patrick@patmurraydev.com>
2016-11-30 20:54:59 -0500

    fixed dates

--- static/countdown.js
+++ static/countdown.js
@@ -1,19 +1,19 @@
 /*THESE ARE REAL VALUES DON'T EDIT*/
-// var hackingStarts = new Date("Dec 03 2016 09:00:00 GMT-0500");
-// var hsCountdown = $(".countdown[data-cdNum='1']");
-// var devpostEnds = new Date("Dec 03 2016 20:00:00 GMT-0500");
-// var deCountdown = $(".countdown[data-cdNum='2']");
-// var hackingEnds = new Date("Dec 03 2016 21:00:00 GMT-0500");
-// var heCountdown = $(".countdown[data-cdNum='3']");
+var hackingStarts = new Date("Dec 03 2016 09:00:00 GMT-0500");
+var hsCountdown = $(".countdown[data-cdNum='1']");
+var devpostEnds = new Date("Dec 03 2016 20:00:00 GMT-0500");
+var deCountdown = $(".countdown[data-cdNum='2']");
+var hackingEnds = new Date("Dec 03 2016 21:00:00 GMT-0500");
+var heCountdown = $(".countdown[data-cdNum='3']");
 
 
 /*THESE ARE FOR TESTING -- COMMENT FOR PROD*/
-var hackingStarts = new Date("Nov 28 2016 21:25:00 GMT-0500");
-var hsCountdown = $(".countdown[data-cdNum='1']");
-var devpostEnds = new Date("Nov 28 2016 21:30:00 GMT-0500");
-var deCountdown = $(".countdown[data-cdNum='2']");
-var hackingEnds = new Date("Nov 28 2016 21:35:00 GMT-0500");
-var heCountdown = $(".countdown[data-cdNum='3']");
+// var hackingStarts = new Date("Nov 28 2016 21:25:00 GMT-0500");
+// var hsCountdown = $(".countdown[data-cdNum='1']");
+// var devpostEnds = new Date("Nov 28 2016 21:30:00 GMT-0500");
+// var deCountdown = $(".countdown[data-cdNum='2']");
+// var hackingEnds = new Date("Nov 28 2016 21:35:00 GMT-0500");
+// var heCountdown = $(".countdown[data-cdNum='3']");
 
 hsCountdown.countdown(hackingStarts.getTime(), {elapse: false, defer: true})
     .on('update.countdown', function(event){
